a709bdc1c9713995611608ae588c499d1138aa1f
LuitelN <84577877+LuitelN@users.noreply.github.com>
2022-10-01 09:58:13 +0545

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+    float USD;
+    float NPR = 130;
+    cout << "Enter the Amount in USD" << endl;
+    cin>>USD;
+    cout<<"USD of "<<USD<<" Nepalese currency is "<<USD*NPR<<endl;
+    return 0;
+}
